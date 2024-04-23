a433caa1a30bad96557127ba17941b4250606b74
Harry <>
2024-04-23 22:13:23 +0200

    primo commit

---
+++ src/main/java/com/mycompany/progetto_azienda/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.progetto_azienda;
+
+/**
+ *
+ * @author taran
+ */
+public class App {
+
+    public static void main(String[] args) {
+        
+    }
+}
